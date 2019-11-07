0dc97cbc327398bfe141ccd2038e95c5942b6320
Tristan McPherson <shredder8910@gmail.com>
2019-11-06 22:20:35 -0500

    Fix bug in Publicizer

--- Publicizer/Program.cs
+++ Publicizer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using Mono.Cecil;
 using MonoMod;
@@ -10,12 +14,14 @@ namespace Publicizer
 	{
 		static void Main(string[] args)
 		{
-			string dllname = @".\Managed\Assembly-CSharp.dll";
+			string basePath = Directory.GetCurrentDirectory();
+			string projectFolder = new DirectoryInfo(basePath).Parent.Parent.Parent.FullName;
+			string dllPath = Path.Combine(projectFolder, "Managed\\Assembly-CSharp.dll");
 
 			using (MonoModder mm = new MonoModder()
 			{
-				InputPath = dllname,
-				OutputPath = dllname.Replace(".dll", "Public.dll"),
+				InputPath = dllPath,
+				OutputPath = dllPath.Replace(".dll", "Public.dll"),
 				ReadingMode = ReadingMode.Deferred
 			})
 			{
@@ -24,34 +30,53 @@ namespace Publicizer
 
 				foreach (var typeDefinition in mm.Module.Types)
 				{
-					typeDefinition.SetPublic(true);
-					foreach (var field in typeDefinition.Fields)
-					{
-						// Ignore compiler generated fields for now,
-						// Maybe check if there is an event with the same name instead
-						if (field.CustomAttributes.Any(c => c.AttributeType.FullName == typeof(CompilerGeneratedAttribute).FullName)) {
-							continue;
+
+					var currentTypes = new Queue<TypeDefinition>();
+					var currentType = typeDefinition;
+
+					do {
+						currentType.SetPublic(currentType.IsPublic);
+
+
+						foreach (var field in currentType.Fields) {
+							// Ignore compiler generated fields for now,
+							// Maybe check if there is an event with the same name instead
+							if (field.CustomAttributes.Any(c => c.AttributeType.FullName == typeof(CompilerGeneratedAttribute).FullName)) {
+								continue;
+							}
+
+							field.SetPublic(true);
 						}
 
-						field.SetPublic(true);
-					}
+						foreach (var ev in currentType.Events) {
+							ev.SetPublic(true);
+						}
 
-					foreach (var ev in typeDefinition.Events) {
-						ev.SetPublic(true);
-					}
+						foreach (var prop in currentType.Properties) {
+							prop.SetPublic(true);
+						}
 
-					foreach (var prop in typeDefinition.Properties) {
-						prop.SetPublic(true);
-					}
+						foreach (var method in currentType.Methods) {
+							method.SetPublic((true));
+						}
+						
 
-					foreach (var method in typeDefinition.Methods)
-					{
-						method.SetPublic((true));
-					}
+
+
+						if (currentType.HasNestedTypes) {
+							foreach (var type in currentType.NestedTypes) {
+								currentTypes.Enqueue(type);
+							}
+						}
+
+						currentType = currentTypes.Count > 0 ? currentTypes.Dequeue() : null;
+					} while (currentType != null);
 				}
-
-				mm.Module.Write(dllname.Replace(".dll", ".Public.dll"));
+				
+				mm.Module.Write(dllPath.Replace(".dll", ".Public.dll"));
 			}
+
+			Console.WriteLine("Finished publicizing code...");
 		}
 	}
 }
